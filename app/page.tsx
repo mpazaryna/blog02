934b8444b334923ac768420c8993db742d0882a6
Matthew Pazaryna <52605785+mpazaryna@users.noreply.github.com>
2024-11-07 22:04:35 -0500

    fix: remove twitter links

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,6 @@ import { socialLinks } from "./config";
 export default function Page() {
   return (
     <section>
-      <a href={socialLinks.twitter} target="_blank">
-        <Image
-          src="/profile.png"
-          alt="Profile photo"
-          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
-          unoptimized
-          width={160}
-          height={160}
-          priority
-        />
-      </a>
-
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
         Portfolio, made simple!
       </h1>
