ed16c4527c188b3beaf5991fac9f0e088b8b408e
Matthew Pazaryna <52605785+mpazaryna@users.noreply.github.com>
2024-11-07 22:00:32 -0500

    fix footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import {
-  FaXTwitter,
   FaGithub,
   FaInstagram,
   FaRss,
@@ -39,14 +38,6 @@ export default function Footer() {
   return (
     <small className="block lg:mt-24 mt-16 text-[#1C1C1C] dark:text-[#D4D4D4]">
       <time>© {YEAR}</time>{" "}
-      <a
-        className="no-underline"
-        href={socialLinks.twitter}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {metaData.title}
-      </a>
       <style jsx>{`
         @media screen and (max-width: 480px) {
           article {
