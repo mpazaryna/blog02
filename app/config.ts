e3578acdf8e74a67a1b7e590343e73db42a68a72
Matthew Pazaryna <52605785+mpazaryna@users.noreply.github.com>
2024-11-07 21:57:43 -0500

    chore: updated footer

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,15 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://blog.paz.land/",
+  title: "Log",
+  name: "Paz",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  github: "https://github.com/mpazaryna",
+  instagram: "https://www.instagram.com/mpazphotos/",
+  linkedin: "https://www.linkedin.com/in/mpazaryna/",
+  email: "mailto:mpazbot@gmail.com",
 };
