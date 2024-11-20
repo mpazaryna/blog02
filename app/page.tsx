9e4df4bcf00a84f56638806f9738fa0c6cfe33ae
Matthew Pazaryna <52605785+mpazaryna@users.noreply.github.com>
2024-11-20 12:48:10 -0500

    updated home

--- app/page.tsx
+++ app/page.tsx
@@ -9,27 +9,8 @@ export default function Page() {
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          A clean, fast, and lightweight portfolio template built with Next.js,
-          Vercel, and Tailwind CSS for optimal performance.
-        </p>
-        <p>
-          Nextfolio includes all the essentials for a stunning portfolio: SEO,
-          MDX support, RSS, Atom, & JSON feeds, analytics, tweet & YouTube
-          embeds, KaTeX integration, and{" "}
-          <a
-            target="_blank"
-            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
-          >
-            more
-          </a>
-          .
-        </p>
-        <p>
-          Nextfolio is{" "}
-          <a href={socialLinks.github} target="_blank">
-            open-source
-          </a>{" "}
-          and fully customizable, making it easy to add more features.
+          I develop thoughtful applications, capture quiet moments through photography, and guide others in finding balance through yoga. 
+          My work seeks to blend technology with mindfulness, creating space for both innovation and stillness.
         </p>
       </div>
     </section>
