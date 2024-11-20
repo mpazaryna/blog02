8ad887f757f12dc6cff3e2af3ef80d554cffde32
Matthew Pazaryna <52605785+mpazaryna@users.noreply.github.com>
2024-11-20 16:11:52 -0500

    adding summary

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -26,15 +26,20 @@ export default function BlogPosts() {
           .map((post) => (
             <Link
               key={post.slug}
-              className="flex flex-col space-y-1 mb-4 transition-opacity duration-200 hover:opacity-80"
+              className="flex flex-col space-y-1 mb-4 transition-all duration-200 hover:bg-[papayawhip] dark:hover:bg-[#574b3c] p-2 rounded-md"
               href={`/blog/${post.slug}`}
             >
-              <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
-                <p className="text-black dark:text-white tracking-tight">
-                  {post.metadata.title}
-                </p>
-                <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
-                  {formatDate(post.metadata.publishedAt, false)}
+              <div className="w-full flex flex-col space-y-1">
+                <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center sm:space-x-2">
+                  <p className="text-black dark:text-white tracking-tight">
+                    {post.metadata.title}
+                  </p>
+                  <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm shrink-0">
+                    {formatDate(post.metadata.publishedAt, false)}
+                  </p>
+                </div>
+                <p className="text-neutral-600 dark:text-neutral-400 text-sm max-w-[85%] line-clamp-2">
+                  {post.metadata.summary}
                 </p>
               </div>
             </Link>
