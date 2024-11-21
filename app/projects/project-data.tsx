7b16867196043452857f0cda0adc63cfb643f53e
Matthew Pazaryna <52605785+mpazaryna@users.noreply.github.com>
2024-11-20 19:53:05 -0500

    feat: add stackwise to the projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -9,18 +9,18 @@ export interface Project {
 export const projects: Project[] = [
   {
     title: "Woodshed AI",
-    year: 2023,
+    year: 2024,
     description:
       "This is a project to help with alignment in software development.",
     url: "https://github.com/mpazaryna/woodshed-ai",
     blog: "woodshed-ai",
   },
   {
-    title: "Alignment Application",
-    year: 2023,
+    title: "Stackwise",
+    year: 2024,
     description:
-      "A tool to help with alignment in software development.",
-    url: "https://yoga.paz.land/",
-    blog: "alignment",
+      "A tool to help build and manage context prompts for LLMs.",
+    url: "https://github.com/mpazaryna/stackwise",
+    blog: "stackwise",
   },
 ];
